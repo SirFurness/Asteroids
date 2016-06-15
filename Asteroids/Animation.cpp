6528074883c0614a3521720cd26fc6eac3af91ee
SirFurness <eric.1334@icloud.com>
2016-06-15 12:16:52 -0500

    Removed folder from repository

---
+++ Asteroids/Animation.cpp
@@ -0,0 +1,9 @@
+//
+//  Animation.cpp
+//  Asteroids
+//
+//  Created by Eric Paul on 6/14/16.
+//  Copyright © 2016 Eric Paul. All rights reserved.
+//
+
+#include "Animation.hpp"
---
+++ Asteroids/Animation.hpp
@@ -0,0 +1,14 @@
+//
+//  Animation.hpp
+//  Asteroids
+//
+//  Created by Eric Paul on 6/14/16.
+//  Copyright © 2016 Eric Paul. All rights reserved.
+//
+
+#ifndef Animation_hpp
+#define Animation_hpp
+
+#include <stdio.h>
+
+#endif /* Animation_hpp */
