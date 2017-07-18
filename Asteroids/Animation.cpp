c063d18d349e9d6d00306c84dbefbc1a7252d41b
SirFurness <eric.1334@protonmail.com>
2017-07-18 01:31:11 -0700

    Cough Cough

--- Asteroids/Animation.cpp
+++ Asteroids/Animation.cpp
@@ -2,9 +2,7 @@
 //  Animation.cpp
 //  Asteroids
 //
-//  Created by Eric Paul on 6/14/16.
-//  Copyright © 2016 Eric Paul. All rights reserved.
-//
+
 
 #include "Animation.hpp"
 #include <SFML/Graphics.hpp>
