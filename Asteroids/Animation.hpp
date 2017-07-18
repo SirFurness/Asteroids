5bd731762c60ae42855420fb6038062779e7310a
SirFurness <eric.1334@protonmail.com>
2017-07-18 01:30:57 -0700

    Cough Cough

--- Asteroids/Animation.hpp
+++ Asteroids/Animation.hpp
@@ -2,9 +2,7 @@
 //  Animation.hpp
 //  Asteroids
 //
-//  Created by Eric Paul on 6/14/16.
-//  Copyright © 2016 Eric Paul. All rights reserved.
-//
+
 
 #ifndef Animation_hpp
 #define Animation_hpp
