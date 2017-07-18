2161788a23cf180ca42cd99d09a87122af3e6b1c
SirFurness <eric.1334@protonmail.com>
2017-07-18 01:24:11 -0700

    Cough Cough

--- Asteroids/game_state_t.hpp
+++ Asteroids/game_state_t.hpp
@@ -1,10 +1,7 @@
 //
 //  game_state_t.hpp
 //  Asteroids
-//
-//  Created by Eric on 5/27/16.
-//  Copyright © 2016 Eric. All rights reserved.
-//
+
 
 #ifndef game_state_t_hpp
 #define game_state_t_hpp
